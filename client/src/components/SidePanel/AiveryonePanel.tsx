ca76abccf96f4bc3f13a71df24f783429cd7de4d
pierrelamon <lamonqdw@gmail.com>
2026-02-02 08:56:10 +0100

    Create AiveryonePanel.tsx

---
+++ client/src/components/SidePanel/AiveryonePanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AiveryonePanel = () => {
+  return (
+    <div className="p-4 bg-red-500 text-white">
+      Test de mon nouveau panneau dans un tsx à part !
+    </div>
+  );
+};
+
+export default AiveryonePanel;
